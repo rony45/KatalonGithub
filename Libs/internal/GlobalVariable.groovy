53242750c5f36a52a94d258d5ccc0321a66b79a5
syahronigit <mrsyahx@gmail.com>
2019-02-14 18:58:33 +0700

    commit pertama katalon

---
+++ Libs/internal/GlobalVariable.groovy
@@ -0,0 +1,23 @@
+package internal
+
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.main.TestCaseMain
+
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+public class GlobalVariable {
+     
+
+    static {
+        try {
+            def selectedVariables = TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
+            selectedVariables += RunConfiguration.getOverridingParameters()
+    
+            
+        } catch (Exception e) {
+            TestCaseMain.logGlobalVariableError(e)
+        }
+    }
+}
---
+++ Test Suites/testsuite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testsuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>88e00fe6-ed20-4ccf-8808-90226bec34f5</testSuiteGuid>
+   <testCaseLink>
+      <guid>161d5ff8-3e24-41c4-bb7a-0377052cf2fe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/testcase1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
